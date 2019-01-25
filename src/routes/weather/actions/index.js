fd060c8d0d1a827698e4342669fd01c348e37323
Majid Mokhtari <majidmokht@gmail.com>
2019-01-25 15:03:07 -0800

    change weather api

--- src/routes/weather/actions/index.js
+++ src/routes/weather/actions/index.js
@@ -5,7 +5,7 @@ import * as types from "../../../constants/types";
 export function getWeather() {
   return dispatch => {
     axios
-      .get("http://localhost:8000/")
+      .get("http://localhost:8000/weather")
       .then(res => {
         return dispatch({
           type: types.WEATHER_IS_SET,
