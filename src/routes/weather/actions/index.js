86540d98958682caca59ff95f6bc142e66d1cfc7
Majid Mokhtari <majidmokht@gmail.com>
2019-01-29 12:18:50 -0800

    add env variable

--- src/routes/weather/actions/index.js
+++ src/routes/weather/actions/index.js
@@ -1,19 +1,22 @@
-import axios from "axios";
-import * as util from "../../auth/components/util";
-import * as types from "../../../constants/types";
+import axios from 'axios'
+import * as util from '../../auth/components/util'
+import * as types from '../../../constants/types'
 
-export function getWeather() {
+export const baseUrl =
+  process.env.REACT_APP_PERSONAL_DASH_URL || 'http://localhost:8000'
+
+export function getWeather () {
   return dispatch => {
     axios
-      .get("http://localhost:8000/weather")
+      .get(`${baseUrl}/weather`)
       .then(res => {
         return dispatch({
           type: types.WEATHER_IS_SET,
           payload: res.data
-        });
+        })
       })
       .catch(err => {
-        return dispatch(util.onServerError(err));
-      });
-  };
+        return dispatch(util.onServerError(err))
+      })
+  }
 }
