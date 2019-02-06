a0ab5d49891c52af16e630c5e76df5bb314f660d
Majid Mokhtari <majidmokht@gmail.com>
2019-02-05 22:40:27 -0800

    remove util extra ocdes

--- src/routes/auth/components/util.js
+++ src/routes/auth/components/util.js
@@ -1,17 +1,7 @@
 import Cookies from 'js.cookie'
-import objectAssign from 'object-assign'
 import * as types from '../../../constants/types'
 
 const COOKIE_PATH = 'PERSONAL_DASH_USER'
-
-export function getParameterByName (name, url) {
-  const cleanName = name.replace(/[\]]/g, '\\$&')
-  const regex = new RegExp(`[?&]${cleanName}(=([^&#]*)|&|#|$)`)
-  const results = regex.exec(url || window.location.href)
-  if (!results) return null
-  if (!results[2]) return ''
-  return decodeURIComponent(results[2].replace(/\+/g, ' '))
-}
 
 export function storeCurrentUser ({ token }) {
   Cookies.set(COOKIE_PATH, token)
@@ -27,42 +17,6 @@ export function getCurrentUser () {
 
 export function logoutUser () {
   Cookies.remove(COOKIE_PATH)
-  return {
-    type: types.USER_LOGGED_OUT
-  }
-}
-
-export function handleAuthResponse (response) {
-  const token = response.headers.get('Authorization')
-  return objectAssign({}, response, {
-    token
-  })
-}
-
-export function handleAuthErrors (response) {
-  if (!response.ok) {
-    if (response.status === 401) {
-      Cookies.remove(COOKIE_PATH)
-      // return nav.routeToLogin();
-    }
-  }
-  return response
-}
-
-export function handleServerErrors (res) {
-  const { status } = res
-  const { errors, message } = status
-  if (message && message !== 'success' && errors.length) {
-    throw Error(`${message}: ${errors[0].message}`)
-  }
-  return res
-}
-
-export function hasError (response) {
-  return (
-    response === null ||
-    (response.is_alert && response.alert_type !== 'success')
-  )
 }
 
 export function onServerError (err) {
